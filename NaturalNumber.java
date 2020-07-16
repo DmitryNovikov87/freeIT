aca5be3222c475aceb4a7817b569d8c82c917de8
DmitryNovikov87 <67970810+DmitryNovikov87@users.noreply.github.com>
2020-07-16 20:06:57 +0300

    Add files via upload

---
+++ NaturalNumber.java
@@ -0,0 +1,14 @@
+public class NaturalNumber {
+    public static void main(String[] args) {
+        int n = 5478;
+        int un = n%10;
+        int ten = (n%100-un)/10;
+        int hund = (n%1000-ten)/100;
+        int thou = (n%10000-hund)/1000;
+        System.out.println(un);
+        System.out.println(ten);
+        System.out.println(hund);
+        System.out.println(thou);
+        System.out.println(un*1000+ten*100+hund*10+thou);
+    }
+}
